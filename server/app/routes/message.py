e1b1b2807168dc699b31d7020cbd7dcdb509c048
kamaukimani <paulkimys285@gmail.com>
2026-09-20 16:30:29 +0300

    add POST PATCH and DELETE http request to routes

--- server/app/routes/message.py
+++ server/app/routes/message.py
@@ -1,20 +1,41 @@
 from flask import Blueprint,request,make_response
 from app.models import Message
+from app.db import db
 
 message_bp=Blueprint("message",__name__)
-@message_bp.route('/all',methods=["GET"])
+@message_bp.route('/all',methods=["GET","POST"])
 def messages():
-    messages=[]
+    if request.method == "GET":
+        messages=[]
     
-    for message in Message.query.all():
+        for message in Message.query.all():
+            message_dict=message.to_dict()
+            messages.append(message_dict)
+        response=make_response(
+            messages,
+            200
+        )
+        return response
+    elif request.method == "POST":
+        data=request.get_json()
+        message=Message()
+        allowed_fields=["body","username"]
+        for attr in data:
+            if attr in allowed_fields:
+                setattr(message,attr,data[attr])
+        db.session.add(message)
+        db.session.commit ()
+
         message_dict=message.to_dict()
-        messages.append(message_dict)
-    response=make_response(
-        messages,
-        200
-    )
-    return response
-@message_bp.route('/<int:id>',methods=["GET"])
+        response=make_response(
+            message_dict,
+            201
+        )
+        return response
+
+
+
+@message_bp.route('/<int:id>',methods=["GET","PATCH","DELETE"])
 def message_by_id(id):
     message=Message.query.filter(Message.id == id).first()
     if message is None:
@@ -22,9 +43,34 @@ def message_by_id(id):
             "message":"OOPss!!!!The record does not exist in our database!!!!"
         }
         return response,404
-    message_dict=message.to_dict()
-    response=make_response(
-        message_dict,
-        200
-    )
-    return response
+    if request.method == "GET":
+        message_dict=message.to_dict()
+        response=make_response(
+            message_dict,
+            200
+        )
+        return response
+    elif request.method == "PATCH":
+        data=request.get_json()
+        allowed_fields=["body"]
+        for attr,value in data.items():
+            if attr in allowed_fields:
+                setattr(message,attr,value)
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict=message.to_dict()
+        response=make_response(
+            message_dict,
+            200
+        )
+        return response
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response={
+            "deleted_successfully":True,
+            "message":"Message deleted successfully"
+        }
+        return make_response(response,200)
