2b78615c59418fa4a6cbebc76abfca5bfef3c278
kamaukimani <paulkimys285@gmail.com>
2026-09-20 13:45:48 +0300

    set up the application to run

---
+++ server/main.py
@@ -0,0 +1,11 @@
+from app import create_app
+from flask_cors import CORS
+
+app=create_app()
+#CORS(app)
+@app.route('/')
+def index():
+    return "Index for Message API"
+
+if __name__=="__main__":
+    app.run(port=5555,debug=True)
