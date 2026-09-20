c2413398654f77901ad9a71f8748b23f638c03bf
kamaukimani <paulkimys285@gmail.com>
2026-09-20 13:47:29 +0300

    initialize the flask app

---
+++ server/app/__init__.py
@@ -0,0 +1,12 @@
+from .db import db,migrate
+from flask import Flask 
+from config import Config
+
+def create_app():
+    app=Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+    return app
