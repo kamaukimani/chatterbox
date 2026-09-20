d20bb017d46fe16d0d2165c4b1f62bbd15758f6d
kamaukimani <paulkimys285@gmail.com>
2026-09-20 16:36:05 +0300

    use flask url to delete a message in the database

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -13,7 +13,8 @@ function Message({ message, currentUser, onMessageDelete, onUpdateMessage }) {
   const isCurrentUser = currentUser.username === username;
 
   function handleDeleteClick() {
-    fetch(`http://127.0.0.1:4000/messages/${id}`, {
+    //fetch(`http://127.0.0.1:4000/messages/${id}`, {
+    fetch(`http://127.0.0.1:5555/messages/${id}`,{
       method: "DELETE",
     });
 
